b6a36eb22d82d31bd33d6cf165b565c3f27849d1
chrish <github@sirchristian.net>
2024-12-02 10:38:30 -0500

    Day 02 Finished

--- 02/day_02_part02.py
+++ 02/day_02_part02.py
@@ -1,51 +1,74 @@
 import re
+VERBOSE = False
+FILE = 'input.txt'
 
-input_file = 'test_input.txt'
-reports= []
-length = 0
-with open(input_file, encoding='utf-8') as f:
-    line = f.readline()
-    while line != '':
-        length = length + 1
-        levels = re.split(r'\W+', line.strip())
-        reports.append(list(map(lambda n: int(n),levels)))
+def read_and_parse_file(input_file = FILE):
+    reports= []
+    with open(input_file, encoding='utf-8') as f:
         line = f.readline()
+        while line != '':
+            levels = re.split(r'\W+', line.strip())
+            reports.append([int(n) for n in levels])
+            line = f.readline()
+    return reports
 
-num_safe_levels = 0
-for x in range(0, length):
-    levels = reports[x]
+def verbose_print(s):
+    if VERBOSE:
+        print(s)
+
+def evaluate_report_safety(levels, report_display, tab=''):
     safe = True
     prev_level = levels[0]
     direction = None
-    dampener_triggered = False
     for level in levels[1:]:
-        if not safe and dampener_triggered:
-            break
-        if not safe and not dampener_triggered:
-            print("Triggered Dampener okay to continue")
-            dampener_triggered = True
-            safe = True
-
+        if not safe:
+            return False
+        
         if direction is None:
             direction = 'increasing' if prev_level < level else 'decreasing' if prev_level > level else None
             if direction is None:
+                verbose_print(f"{tab}Report {report_display} is unsafe because direction changed between {prev_level} and {level}")
                 safe = False
+                
         dist = level - prev_level
         if dist == 0:
-            print(f"Report #{x+1} is unsafe because dist between {level} and {prev_level} is 0")
+            verbose_print(f"{tab}Report {report_display} is unsafe because dist between {prev_level} and {level} is 0")
             safe = False
         elif abs(dist) > 3:
-            print(f"Report #{x+1} is unsafe because dist between {level} and {prev_level} is >3")
+            verbose_print(f"{tab}Report {report_display} is unsafe because dist between {prev_level} and {level} is >3")
             safe = False
         elif direction == 'increasing' and dist < 0:
-            print(f"Report #{x+1} is unsafe because {level} and {prev_level} are decreasing but direction was increasing")
+            verbose_print(f"{tab}Report {report_display} is unsafe because {prev_level} and {level} are decreasing but direction was increasing")
             safe = False
         elif direction == 'decreasing' and dist > 0:
-            print(f"Report #{x+1} is unsafe because {level} and {prev_level} are increasing but direction was decreasing")
+            verbose_print(f"{tab}Report {report_display} is unsafe because {prev_level} and {level} are increasing but direction was decreasing")
             safe = False
         prev_level = level
 
-    num_safe_levels = num_safe_levels if not safe else num_safe_levels + 1
-    print(f'Report {x+1} has {len(levels)} levels: {levels}. And is {"safe" if safe else "unsafe"}')
+    verbose_print(f'{tab}Report {report_display} has {len(levels)} levels: {levels}. And is {"safe" if safe else "unsafe"}')
+    return safe
 
-print(num_safe_levels)
+
+def get_number_of_safe_levels(reports):
+    length = len(reports)
+    num_safe_levels = 0
+    for x in range(0, length):
+        levels = reports[x]
+        verbose_print(f"Checking report #{x+1}...")
+        if evaluate_report_safety(levels, f"#{x+1}-{levels}", "\t"):
+            num_safe_levels += 1
+        else:
+            verbose_print(f"\tChecking subset | {levels}")
+            num_safe_subset = 0
+            for i in range(1, len(levels)+1):
+                level_minus_one = levels[:i-1] + levels[i:]
+                if evaluate_report_safety(level_minus_one, f"#{x+1}.{i}-{level_minus_one}", "\t\t"):
+                    num_safe_subset += 1
+            if num_safe_subset >= 1:
+                verbose_print(f"\tReport #{x+1} is safe because removing one level makes it safe")
+                num_safe_levels += 1
+                
+    return num_safe_levels
+
+reports = read_and_parse_file()
+print(get_number_of_safe_levels(reports))
