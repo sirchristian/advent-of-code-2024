59a63421e45b77844a80acc81db7f43b13efa40f
chrish <github@sirchristian.net>
2024-12-03 13:13:40 -0500

    day 3 part 2

--- 03/day_03_part02.py
+++ 03/day_03_part02.py
@@ -8,10 +8,33 @@ def read_file(input_file = FILE):
 def mul(a, b):
     return a * b
 
-pattern = re.compile(r'mul\(\d{0,3},\d{0,3}\)')
+mul_pattern = re.compile(r'mul\(\d{0,3},\d{0,3}\)')
 
 input = read_file()
+muls = []
+until_dont_pattern = re.compile(r'(?P<muls>.*?)(don\'t\(\))(?P<rest>.*)', re.M | re.S)
+until_do_pattern = re.compile(r'.*?(do\(\))(?P<rest>.*)', re.M | re.S)
+
+capture_rest = True
+while input:
+    search_until_dont = until_dont_pattern.search(input)
+    if search_until_dont:
+        g = search_until_dont.groupdict()
+        if capture_rest:
+            muls = muls + [f for f in mul_pattern.findall(g['muls'])]
+        input = g['rest']
+        capture_rest = False
+    else:
+        if capture_rest:
+            muls = muls + [f for f in mul_pattern.findall(input)]
+        break
+    
+    search_until_do = until_do_pattern.search(input)
+    if search_until_do:
+        capture_rest = True
+        g = search_until_do.groupdict()
+        input = g['rest']
 
 local_vals = {}
-answer = sum(exec('x = ' + f, {'mul': mul}, local_vals) or local_vals['x'] for f in [f for f in pattern.findall(input)])
+answer = sum(exec('x = ' + m, {'mul': mul}, local_vals) or local_vals['x'] for m in muls)
 print(answer)
