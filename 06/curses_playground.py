8b96b7f9b6b48afddb458c2e05aa01826294b577
chrish <github@sirchristian.net>
2024-12-06 11:29:40 -0500

    Experimenting with a curses game board

--- 06/curses_playground.py
+++ 06/curses_playground.py
@@ -6,8 +6,9 @@ BOARD_WIDTH = 25
 BOARD_HEIGHT = 25
 NUM_X = 10  # Number of random 'X' characters to place on the board
 
-def game_loop(stdscr):
+def game_loop(stdscr, start_pos_y, start_pos_x, start_direction_char):
     num_dots_eaten = 0
+    direction = start_direction_char
     screen_h, screen_w = stdscr.getmaxyx()
     view_height = min(screen_h, BOARD_HEIGHT)
     view_width = min(screen_w, BOARD_WIDTH)
@@ -33,7 +34,7 @@ def game_loop(stdscr):
     # Game loop
     while True:
         # Move the cursor to its current position
-        pad.addch(cursor_y, cursor_x, '^')  # Mark cursor
+        pad.addch(cursor_y, cursor_x, direction)  # Mark cursor
         pad.refresh(0, 0, 0, 0, view_height - 1, view_width - 1)
 
         # Wait for user input
@@ -41,12 +42,16 @@ def game_loop(stdscr):
 
         new_y, new_x = cursor_y, cursor_x
         if key == curses.KEY_UP and new_y > 0:
+            direction = '^'
             new_y -= 1
         elif key == curses.KEY_DOWN and new_y < BOARD_HEIGHT - 1:
+            direction = 'v'
             new_y += 1
         elif key == curses.KEY_LEFT and new_x > 0:
+            direction = '<'
             new_x -= 1
         elif key == curses.KEY_RIGHT and new_x < BOARD_WIDTH - 1:
+            direction = '>'
             new_x += 1
         elif key == ord('q'):  # Quit the game
             return num_dots_eaten
